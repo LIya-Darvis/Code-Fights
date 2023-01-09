45ae36663ef4cb94d914305635be6c00f53ab722
Liya Darvis <anastasiaadm2019@gmail.com>
2023-01-09 16:49:42 +0300

    comments
    a lot of comments

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 import './style.css';
 
 import * as THREE from 'https://unpkg.com/three@0.148.0/build/three.module.js';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+// =========== КОНСТАНТЫ ===========
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
-
-// console.log(scene);
-// console.log(camera);
-// console.log(renderer);
-
-renderer.setSize(innerWidth, innerHeight);
-document.body.appendChild(renderer.domElement);
-renderer.render(scene, camera);
-
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// =========== ОБЪЕКТЫ ===========
+
+const orbit = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x22fafa });
@@ -26,17 +22,23 @@ const box = new THREE.Mesh(geometry, material);
 scene.add(box);
 
 
-const orbit = new OrbitControls(camera, renderer.domElement);
 
+
+// =========== ОСИ КООРДИНАТ ===========
 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-camera.position.set(4, 1, 4);
+// =========== КАМЕРА ===========
 
+camera.position.set(-10, 30, 30);
 orbit.update();
 
+// =========== ФИНАЛЬНЫЙ РЕНДЕР ===========
+
 renderer.render(scene, camera);
+
+// =========== АНИМАЦИЯ ===========
 
 function animate() {
   requestAnimationFrame(animate);
